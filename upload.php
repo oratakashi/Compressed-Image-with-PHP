8633a9bb523bb3033355a0fdd9a8ef722d2f53a0
Oratakashi Nhamako <davidcreazy@gmail.com>
2020-06-03 11:19:46 +0700

    Update PNG Support

--- upload.php
+++ upload.php
@@ -1,8 +1,5 @@
 <?php
-$foto_tmp = $_FILES['foto']['tmp_name'];
-$foto_name = $_FILES['foto']['name'];
 $foto_type = $_FILES['foto']['type'];
-$foto_size = $_FILES['foto']['size'];
 
 $directory      = "./media/source/";
 $directory_compress      = "./media/compressed/";
@@ -10,18 +7,44 @@ $directory_compress      = "./media/compressed/";
 $tgl = date("YmdHis");
 $file = $directory . $tgl;
 
-//Simpan Gambar Ukuran Asli
-$realImagesName = $_FILES['foto']['tmp_name'];
+/**
+ * Mengubah Base 64 Disik
+ */
+$data = file_get_contents($_FILES['foto']['tmp_name']);
+$type = explode("/", $foto_type);
+
+/**
+ * Di Anggap File Sudah base64
+ */
+$base64_encode = 'data:image/' . $type[1] . ';base64,' . base64_encode($data);
+
+$img_base64 = explode(";base64,", $base64_encode);
+
+/**
+ * Mengambil meta data dari base64
+ */
+$f = finfo_open();
+
+$base64_type_files = finfo_buffer($f, base64_decode($img_base64[1]), FILEINFO_MIME_TYPE);
+$type = explode("/", $base64_type_files);
 
 //Upload File Asli
-move_uploaded_file($realImagesName, $file);
+file_put_contents($file . "." . $type[1], base64_decode($img_base64[1]));
 
 //Identitas File Gambar
-$realImages = imagecreatefromjpeg($file);
+$realImages = "";
+if ($type[1] == "png") {
+  $realImages = imagecreatefrompng($file . "." . $type[1]);
+} else {
+  $realImages = imagecreatefromjpeg($file . "." . $type[1]);
+}
 $width = imageSX($realImages);
 $height = imageSY($realImages);
 
-//Menentukan Ukuran Compressed Gambar
+/**
+ * Menentukan Ukuran Compressed Gambar
+ * 75 itu 75% tinggal di ganti sesuai kebutuhan
+ */
 $img_width = (75 / 100) * $width;
 $img_height = ($img_width / $width) * $height;
 
@@ -32,11 +55,11 @@ imagecopyresampled($compressed_img, $realImages, 0, 0, 0, 0, $img_width, $img_he
 //Simpan Gambar Thumbnail
 imagejpeg($compressed_img, $directory_compress . "compressed_" . $tgl . ".jpeg");
 
-//Menghapus Objek Dalam Memory
+// //Menghapus Objek Dalam Memory
 imagedestroy($realImages);
 imagedestroy($compressed_img);
 
-//Menghapus Image Asli
-unlink($file);
+// //Menghapus Image Asli
+unlink($file . "." . $type[1]);
 
 echo "Sukses";
